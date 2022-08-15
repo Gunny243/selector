0c0fa542f22fc7d89aa95e83e5c30f71e26150f5
Manicharan <charan24394@gmail.com>
2022-08-15 16:52:21 +0530

    selector

---
+++ app.js
@@ -0,0 +1,26 @@
+// Queryselector and QueryselectorAll 
+
+
+heading = document.querySelector('h1');
+all_h1 = document.querySelectorAll('h1');
+elements_by_Class = document.querySelectorAll('.a2');
+elements_by_id = document.querySelector('#z');
+
+
+
+// getElementById
+
+h4 = document.getElementById('z');
+
+
+// getElementsByClass 
+
+element = document.getElementsByClassName('a2');
+
+// getElementsByTagName
+
+element = document.getElementsByTagName('h1');
+
+// getElementsByName
+
+input = document.getElementsByName('first-name');
